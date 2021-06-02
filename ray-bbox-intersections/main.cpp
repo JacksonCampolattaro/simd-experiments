bfba6614ee2b7bf717dddf6354ec3b2a573f5015
jackcamp <jackcamp@vt.edu>
2021-06-02 19:04:19 -0400

    Generate a random ray and bounding box

--- ray-bbox-intersections/main.cpp
+++ ray-bbox-intersections/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <random>
+#include <chrono>
 
 #include "vector3.h"
 #include "ray.h"
@@ -6,5 +8,22 @@
 #include "intersection_strategies/smits_method.h"
 
 int main() {
-    std::cout << "Hello, world!";
+
+    std::uniform_real_distribution<double> generator(0.000001, 1.0);
+    auto re = std::default_random_engine(std::chrono::system_clock::now().time_since_epoch().count());
+
+    Vector3 origin = {generator(re), generator(re), generator(re)};
+    Vector3 direction = {origin.x() + generator(re), origin.y() + generator(re), origin.z() + generator(re)};
+
+    Ray ray = {origin, direction};
+
+    Vector3 min = {generator(re), generator(re), generator(re)};
+    Vector3 max = {min.x() + generator(re), min.y() + generator(re), min.z() + generator(re)};
+
+    BBox bbox = {min, max};
+
+
+    std::cout << intersect_smits_method(bbox, ray,
+                                        -std::numeric_limits<double>::infinity(),
+                                        std::numeric_limits<double>::infinity());
 }
